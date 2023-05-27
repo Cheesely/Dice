0c92bcd6e6e089d3b819f0371c085abc85f9a37b
chelseyfoster <cfoster@udutu.com>
2023-05-27 15:21:51 -0400

    Set up classes for each roll
    Set up CSS classes to represent each of the possible roll choices

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".cube").forEach((element) => {
         element.addEventListener("click", function() {
             this.classList.remove('spin');
+            this.classList.add('two');
+
         });
     });
 });

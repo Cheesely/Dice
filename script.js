3a75f03813fabb52db6de8126ec5ab9c125403b4
chelseyfoster <cfoster@udutu.com>
2023-06-01 18:32:55 -0400

    click to reroll

--- script.js
+++ script.js
@@ -6,25 +6,36 @@ initializeLightnessPicker();
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".cube").forEach((element) => {
         element.addEventListener("click", async function() {
-            const diceRoll = await rollDice();
-            if(diceRoll == 1){
-                this.classList.remove('spin');
-                this.classList.add('one');
-            } else if(diceRoll == 2){
-                this.classList.remove('spin');
-                this.classList.add('two');
-            } else if(diceRoll == 3){
-                this.classList.remove('spin');
-                this.classList.add('three');
-            } else if(diceRoll == 4){
-                this.classList.remove('spin');
-                this.classList.add('four');
-            } else if(diceRoll == 5){
-                this.classList.remove('spin');
-                this.classList.add('five');
-            } else if(diceRoll == 6){
-                this.classList.remove('spin');
-                this.classList.add('six');
+            if (this.classList.contains('spin')){
+                const diceRoll = await rollDice();
+                if(diceRoll == 1){
+                    this.classList.remove('spin');
+                    this.classList.add('one');
+                } else if(diceRoll == 2){
+                    this.classList.remove('spin');
+                    this.classList.add('two');
+                } else if(diceRoll == 3){
+                    this.classList.remove('spin');
+                    this.classList.add('three');
+                } else if(diceRoll == 4){
+                    this.classList.remove('spin');
+                    this.classList.add('four');
+                } else if(diceRoll == 5){
+                    this.classList.remove('spin');
+                    this.classList.add('five');
+                } else if(diceRoll == 6){
+                    this.classList.remove('spin');
+                    this.classList.add('six');
+                }
+            }
+            else{
+                this.classList.remove('one');
+                this.classList.remove('two');
+                this.classList.remove('three');
+                this.classList.remove('four');
+                this.classList.remove('five');
+                this.classList.remove('six');
+                this.classList.add('spin');
             }
 
         });
